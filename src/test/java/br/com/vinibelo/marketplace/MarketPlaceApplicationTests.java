ed9009e865538859266afee917d3904c740b067a
ViniBelo <vinialvesbelo@outlook.com>
2025-07-28 20:51:15 -0300

    Initial commit

---
+++ src/test/java/br/com/vinibelo/marketplace/MarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinibelo.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
