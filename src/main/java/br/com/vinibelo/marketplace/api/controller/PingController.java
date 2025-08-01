119881fd8f56944d9fd39261a7012889b8a3b102
ViniBelo <vinialvesbelo@outlook.com>
2025-08-01 19:48:56 -0300

    Ping route to check application's health

---
+++ src/main/java/br/com/vinibelo/marketplace/api/controller/PingController.java
@@ -0,0 +1,15 @@
+package br.com.vinibelo.marketplace.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "ping")
+public class PingController {
+    @GetMapping
+    public ResponseEntity<String> ping () {
+        return ResponseEntity.ok("pong");
+    }
+}
